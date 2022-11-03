1840034b4f9f089fb19a7561be52827da4a6063c
joshuam8 <69640687+joshuam8@users.noreply.github.com>
2022-11-02 21:30:06 -0600

    Update consola.js

--- routes/consola.js
+++ routes/consola.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const consolaController= require('../controllers/consola')
 //Create,Read,Update,Delete  (CRUD)  Altas Bajas Cambios Consultas
 //Servicio para mostrar el formulario
-router.get('/altaConsola',consolaController.getAltaConsola);
+//router.get('/altaConsola',consolaController.getAltaConsola);
+
 //Servicio para procesar los datos del formulario
 router.post('/altaConsola',consolaController.postAltaConsola);
+
 //Servicio para consultar todos los datos
 router.get('/consultaConsolas',consolaController.getConsolas);
+
 //Servicio para eliminar un registro por id
-router.post('/bajaConsola', consolaController.postEliminarConsola);
+//router.post('/bajaConsola', consolaController.postEliminarConsola);
+
 //Servicio para actualizar las consola
-router.post('/actualizarConsola',consolaController.postActualizarConsola);
+//router.post('/actualizarConsola',consolaController.postActualizarConsola);
 
 module.exports = router
